ec9cdc211285324082aa4b23bdbd8a0aef96a309
Andrei Scafi <andreiscafi@hotmail.com>
2023-06-18 09:29:27 -0300

    Adding functionality to navbar with Javascript.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /* My Code */
-const menu = document.querySelector('.menu');
+/* const menu = document.querySelector('.menu');
 const navBar = document.querySelector('.navbar');
 const navList = document.querySelector('.nav-list');
 
@@ -7,7 +7,20 @@ menu.addEventListener('click', () => {
     console.log('clicked')
     navBar.classList.toggle('change');
     navList.classList.toggle('change');
-})
+}) */
 /* End of My Code */
+
 /* Teacher Code */
+
+/* Navbar */
+
+const menu = document.querySelector('.menu');
+const navbar = document.querySelector('.navbar');
+
+menu.addEventListener('click', () => {
+    navbar.classList.toggle('change');
+})
+
+/* End of Navbar */
+
 /* End of Teacher Code */
