d90f9f8f5623dca618502c8f34159c4a7412e3fb
Andrei Scafi <andreiscafi@hotmail.com>
2023-06-18 08:57:21 -0300

    My solution to Navbar function.

---
+++ script.js
@@ -0,0 +1,13 @@
+/* My Code */
+const menu = document.querySelector('.menu');
+const navBar = document.querySelector('.navbar');
+const navList = document.querySelector('.nav-list');
+
+menu.addEventListener('click', () => {
+    console.log('clicked')
+    navBar.classList.toggle('change');
+    navList.classList.toggle('change');
+})
+/* End of My Code */
+/* Teacher Code */
+/* End of Teacher Code */
